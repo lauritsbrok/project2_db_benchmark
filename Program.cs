2e094818fe9f7b131a64fbfb04158a099baa945f
Laurits <laku@itu.dk>
2025-04-08 14:41:26 +0200

    add DotNetEnv, sln file and MongoDB.Driver

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using DotNetEnv;
+
+var username = Environment.GetEnvironmentVariable("MONGO_INITDB_ROOT_USERNAME");
+var password = Environment.GetEnvironmentVariable("MONGO_INITDB_ROOT_PASSWORD");
+
+var client = new MongoClient("mongodb://localhost:27017");
+var database = client.GetDatabase("foo");
+var collection = database.GetCollection<BsonDocument>("bar");
+
+await collection.InsertOneAsync(new BsonDocument("Name", "Jack"));
+
+var list = await collection.Find(new BsonDocument("Name", "Jack"))
+    .ToListAsync();
+
+foreach(var document in list)
+{
+    Console.WriteLine(document["Name"]);
+}
