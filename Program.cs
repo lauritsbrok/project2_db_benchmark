e26edf02a0594c850fac2ca916acadd920289e14
Laurits Brok <labp@itu.dk>
2025-04-24 16:14:29 +0200

    m

--- Program.cs
+++ Program.cs
@@ -8,9 +8,10 @@ Env.Load();
 
 // Initialize MongoDB helper
 Globals.Init();
-PostgresDatabaseHelper postgresHelper = new();
 MongoImportHelper mongoImportHelper = new();
+PostgresImportHelper postgresImportHelper = new();
 
 Console.WriteLine("Inserting into Mongo DB...");
 await mongoImportHelper.ImportJsonFiles();
+await postgresImportHelper.ImportJsonFiles();
 Console.WriteLine($"All data inserted in Mongo DB");
