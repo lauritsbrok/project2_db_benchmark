0769441455f768f74c5f5d86d159e0f3fc5f4622
Laurits <laku@itu.dk>
2025-04-29 13:30:15 +0200

    correct program

--- Program.cs
+++ Program.cs
@@ -8,12 +8,14 @@ Env.Load();
 // Initialize MongoDB helper
 Globals.Init();
 MongoImportHelper mongoImportHelper = new();
-// PostgresImportHelper postgresImportHelper = new();
+PostgresImportHelper postgresImportHelper = new();
 
 Console.WriteLine("Benchmarking insert in Mongo DB ...");
 var (totalTime, throughput, latencies) = await mongoImportHelper.BenchmarkInsert();
 Console.WriteLine($"Mongo insert took {totalTime} seconds and had an average throughput of {throughput} tuples pr second");
 Console.WriteLine($"Average Latency: {latencies.Average():F2} ms");
-// await postgresImportHelper.ImportJsonFiles();
+
+Console.WriteLine("Benchmarking insert in Postgres ...");
+var postgres_insert_benchmark = await postgresImportHelper.LoadAndInsert();
 Console.WriteLine($"All data inserted in Mongo DB");
 
